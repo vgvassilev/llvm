4d80ecfeb643fea562431f264b4e74922e62ad7f
Axel Naumann <Axel.Naumann@cern.ch>
2016-06-09 15:55:11 +0200

    Mark weak symbols weak (apparently NFC).

--- lib/ExecutionEngine/RuntimeDyld/RuntimeDyld.cpp
+++ lib/ExecutionEngine/RuntimeDyld/RuntimeDyld.cpp
@@ -603,7 +603,7 @@ void RuntimeDyldImpl::emitWeakSymbols(const ObjectFile &Obj,
     section_iterator SI = SymAndSection.second;
 
     uint32_t Flags = Sym.getFlags();
-    JITSymbolFlags RTDyldSymFlags = JITSymbolFlags::None;
+    JITSymbolFlags RTDyldSymFlags = JITSymbolFlags::Weak;
     if (Flags & SymbolRef::SF_Exported)
       RTDyldSymFlags |= JITSymbolFlags::Exported;
 
